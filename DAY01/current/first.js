bd5c7719cc3a36d16c9d6e4cfec1d9f0e39a0118
aksh31at <asharma21_be22@thapar.edu>
2025-06-07 23:28:23 +0530

    first commit

---
+++ DAY01/current/first.js
@@ -0,0 +1,7 @@
+const {sum, sub, mul} = require('../module');
+
+sum(10, 20);
+sub(20, 10);    
+mul(10, 20);
+
+console.log("hello");
---
+++ DAY01/current/module/index.js
@@ -0,0 +1,5 @@
+const sum= require("../module/sum.js");
+const sub = require("../module/sub.js");    
+const mul = require("../module/mul.js");
+
+module.exports = {sum, sub, mul};
