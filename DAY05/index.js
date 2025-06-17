fed1da9e9edc22109c09afeb645a74b3a63e19eb
aksh31at <asharma21_be22@thapar.edu>
2025-06-18 03:00:14 +0530

    progressDay5

--- DAY05/index.js
+++ DAY05/index.js
@@ -1,25 +1,59 @@
 const express = require('express');
 const app=express();
 
+app.use(express.json());
+
+const Bookstore=[
+    {id:1, name:"Harry Potter", author:"JK Rowling"},
+    {id:2, name:"The Hobbit", author:"JRR Tolkien"},
+    {id:3, name:"1984", author:"George Orwell"},
+    {id:4, name:"To Kill a Mockingbird", author:"Harper Lee"},
+]
+
+
+app.get("/book", (req,res)=>{
+    res.send(Bookstore);
+})
+
+app.get("/book/:id", (req, res) => {
+    const id= parseInt(req.params.id);
+    const Book= Bookstore.find(info=> info.id === id);
+    res.send(Book);
+})
+
+app.post("/book", (req, res) => {
+    Bookstore.push(req.body);
+    res.send("Book added successfully");
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+
+
+
+
+
+
+
 // app.use("/user", (req, res) => {
 //     console.log(req.query);
 //     res.send("You are on User Page");
 // });
 
 
-//parsing
-app.use(express.json());
-//middleware to parse JSON bodies->JS objects
+// //parsing
+// app.use(express.json());
+// //middleware to parse JSON bodies->JS objects
 
-app.get("/user", (req, res) => {
-    res.send({name: "John", age: 30});
-})
+// app.get("/user", (req, res) => {
+//     res.send({name: "John", age: 30});
+// })
 
-app.post("/user", (req, res) => {
-    console.log(req.body);
-    res.send("User data received");
-});
+// app.post("/user", (req, res) => {
+//     console.log(typeof req.body.age);
+//     res.send("User data received");
+// });
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
